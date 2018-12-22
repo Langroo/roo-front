a30d7992300e8f9d20df1b5896b722580e5238b3
Andres Hevia <eternal.aj@gmail.com>
2018-12-22 13:42:04 +0100

    Improved input handler

--- src/input.handler.js
+++ src/input.handler.js
@@ -3,35 +3,34 @@ const getEntityAndFlow = (exp) => {
   const flows = require('./contexts');
 
   // -- Define previous entities
-  let entityAndFlow = { entity: undefined, flow: 'opentalk' };
   let handler;
 
   handler = flows.general_NLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'general' };
+    return { entity: handler, flow: 'general' };
   }
   handler = flows.introduction_NLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'introduction' };
+    return { entity: handler, flow: 'introduction' };
   }
   handler = flows.surveyNLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'survey' };
+    return { entity: handler, flow: 'survey' };
   }
   handler = flows.tutor_NLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'tutor' };
+    return { entity: handler, flow: 'tutor' };
   }
   handler = flows.opentalkNLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'opentalk' };
+    return { entity: handler, flow: 'opentalk' };
   }
   handler = flows.contentNLP(exp);
   if (handler) {
-    return entityAndFlow = { entity: handler, flow: 'content' };
+    return { entity: handler, flow: 'content' };
   }
 
-  return entityAndFlow;
+  return { entity: undefined, flow: 'opentalk' };
 };
 
 module.exports = {
